b456b722854e7f9db64acc27fe95ed6d1381fed6
medrimonia <medrimonia@gmail.com>
2020-04-25 20:58:54 +0200

    ssl_dynamic_ball_approach: update of specific plot tool
    - Reading problem parameters from json file (no comments allowed)
    - Basing argument handling on opt-parse
    - Adding kick/finish area depending on the 'mode'
    - Cleaning most of obsolete content

--- plots/specific/ssl_dynamic/plot.r
+++ plots/specific/ssl_dynamic/plot.r
@@ -1,25 +1,33 @@
 source("../../plot_tools.r")
+library(optparse)
+library(rjson)
+
+# TODO
+# - Add an auto for space size
+# - Remove drawing parameters or use them with cmd line
+# - Print one out of 'n' entries
 
 # Drawing parameters
-
 # TODO: unit should be specified in [m]
-ballSize <- 1.2 # point size -> unit relative to graph size
+ballSize <- 0.3 # point size -> unit relative to graph size
 targetSize <- 5 # point size -> unit relative to graph size
 
-mkCircle <- function(center = c(0,0),diameter = 1, npoints = 100){
-    r = diameter / 2
+# Convert a data frame with coordinate in world basis to robot basis
+robotFromWorld <- function(data, robot_x, robot_y, robot_dir) {
+    data.frame(x = robot_x + data$x * cos(robot_dir) - data$y * sin(robot_dir),
+               y = robot_y + data$x * sin(robot_dir) + data$y * cos(robot_dir))
+}
+
+mkRobot <- function(config, npoints=100){
     tt <- seq(0,2*pi,length.out = npoints)
-    xx <- center[1] + r * cos(tt)
-    yy <- center[2] + r * sin(tt)
+    xx <- config$collision_radius* cos(tt)
+    yy <- config$collision_radius* sin(tt)
+    xx[which(xx > config$collision_forward)] <- config$collision_forward
     return(data.frame(x = xx, y = yy))
 }
 
-mkRobot <- function(robotRadius, collisionForward, npoints=100){
-    tt <- seq(0,2*pi,length.out = npoints)
-    xx <- robotRadius * cos(tt)
-    yy <- robotRadius * sin(tt)
-    xx[which(xx > collisionForward)] = collisionForward
-    return(data.frame(x = xx, y = yy))
+mkRobotWithPose <- function(config, robotX, robotY, robotDir, npoints=100){
+    robotFromWorld(mkRobot(config, npoints), robotX, robotY, robotDir)
 }
 
 mkRect <- function(center = c(0,0),dx = 1, dy = 1)
@@ -31,21 +39,21 @@ mkRect <- function(center = c(0,0),dx = 1, dy = 1)
     return(data.frame(x = xx, y = yy))
 }
 
-mkKickArea <- function(kick)
+mkKickArea <- function(config)
 {
-    mkRect(kick$center, kick$xTol, kick$yTol);
+    dx <- config$kick_x_limits[2] - config$kick_x_limits[1]
+    center <- c(mean(config$kick_x_limits), 0)
+    mkRect(center, dx/2, config$kick_y_tol);
 }
 
-
-
-mkDirOkZone <- function(tolerance, x_max) {
-    dy <- sin(tolerance) * x_max
-    xx <- c(0,x_max,x_max)
-    yy <- c(0,dy,-dy)
-    return(data.frame(x = xx, y = yy))
+mkFinishArea <- function(config)
+{
+    dx <- config$finish_x_limits[2] - config$finish_x_limits[1]
+    center <- c(mean(config$finish_x_limits), 0)
+    mkRect(center, dx/2, config$finish_y_tol);
 }
 
-# Create the triangle going 
+# Create the triangle going from the robot and including all the acceptable position for the target
 # dir_length: the length of the target vector
 mkDirOkZoneTarget <- function(kick_tol, robot_x, robot_y, robot_dir, dir_length = 100) {
     nominal_x <- cos(robot_dir) * dir_length
@@ -59,14 +67,20 @@ mkDirOkZoneTarget <- function(kick_tol, robot_x, robot_y, robot_dir, dir_length 
 }
 
 # Plot the trajectory of the robot in target referential
-vectorPlotTarget <- function(data, variables, outputPath)
+vectorPlotTarget <- function(data, variables, outputPath, config)
 {
-    dt <- 0.1
-    robot_arrow_length <- 0.05
+    # Properties
+    collisionRadius <- config$collision_radius # in [m]
+    collisionForward <- config$collision_forward # in [m]
+    dt <- config$dt
+    robot_arrow_length <- 0.05 # TODO -> param
+    # Computing data in target basis
     c_dir <- cos(data$robot_dir)
     s_dir <- sin(data$robot_dir)
     vecData <- data.frame(
         "robot_dir" = data$robot_dir,
+        "target_x" = 0,
+        "target_y" = 0,
         "robot_x" = -c_dir * data$target_x + s_dir * data$target_y,
         "robot_y" = -s_dir * data$target_x - c_dir * data$target_y,
         "time" = data$step * dt,
@@ -80,94 +94,64 @@ vectorPlotTarget <- function(data, variables, outputPath)
     lastEntry <- tail(vecData,1)
 
     # Centring everything in position of last ball
-    # TODO
+    offset_x <- lastEntry$ball_x
+    offset_y <- lastEntry$ball_y
+    for (prefix in c("robot","robot_end","ball","target"))
+    {
+        for (suffix in c("x","y")) {
+            col <- sprintf("%s_%s",prefix,suffix)
+            offset <- ifelse(suffix == "x", offset_x, offset_y)
+            vecData[,col] <- vecData[,col] - offset
+        }
+    }
+
+    # Updating lastEntry
+    lastEntry <- tail(vecData,1)
         
-    dirOkColor      <- cbbPalette[2]
+    collisionColor <- cbbPalette[1]
+    dirOkColor     <- cbbPalette[2]
+    kickColor      <- cbbPalette[3]
     stepMinColor   <- cbbPalette[4]
     stepMaxColor   <- cbbPalette[5]
     # plotting
     x_var <- "x"
     y_var <- "y"
     g <- ggplot(vecData)
-    # plot target in robot referential
-    g <- g + geom_segment(aes(x=robot_x,y=robot_y, xend = robot_end_x, yend = robot_end_y,
-                              color = time),
-                          arrow = arrow(length = unit(0.05,"cm"))
-                          )
-    g <- g + geom_point(aes(x=ball_x,y=ball_y,
-                            color = time))
+    # Adding kick area
+    okZone <- NULL
+    if (config$mode == "Wide") {
+        okZone <- robotFromWorld(mkFinishArea(config),
+                                 lastEntry$robot_x, lastEntry$robot_y, lastEntry$robot_dir)
+    } else if (config$mode == "Finish") {
+        okZone <- robotFromWorld(mkKickArea(config),
+                                 lastEntry$robot_x, lastEntry$robot_y, lastEntry$robot_dir)
+    }
+    g <- g + geom_polygon(aes(x=x,y=y), okZone, size = 0,
+                          fill= kickColor, alpha=0.5)
     # Adding direction
     dirOkZone <- mkDirOkZoneTarget(lastEntry$kick_dir_tol, lastEntry$robot_x, lastEntry$robot_y,
                                    lastEntry$robot_dir)
     g <- g + geom_polygon(aes(x=x,y=y), dirOkZone, size = 0,
                           fill= dirOkColor, alpha=0.5)
-    # Setting axis
-    g <- g + scale_x_continuous(name = "x [m]",
-                                breaks = variables[[x_var]][["breaks"]],
-                                labels = variables[[x_var]][["labels"]])
-    g <- g + scale_y_continuous(name = "y [m]",
-                                breaks = variables[[y_var]][["breaks"]],
-                                labels = variables[[y_var]][["labels"]])
-    g <- g + scale_color_gradient(name="time [s]",
-                                  low = stepMinColor, high = stepMaxColor)
-    g <- g + coord_cartesian(xlim = variables[[x_var]][["limits"]],
-                             ylim = variables[[y_var]][["limits"]])
-
-    # Set theme
-    g <- g + theme_bw()
-    ggsave(file = outputPath, g,width=5,height=5)
-}
-vectorPlot <- function(data, variables, outputPath)
-{
-    # Properties
-    collisionRadius <- 0.12 # in [m]
-    collisionForward <- 0.09# in [m]
-    # Kick
-    kick <- list(center = c(0.11, 0),
-                 xTol = 0.02,
-                 yTol = 0.05)
-    kickArea <- mkKickArea(kick);
-    
-    x_var <- "x"
-    y_var <- "y"
-    # Calculating temporary variables
-    vecData <- data
-    # Computing bounded target
-    vecData["targetDist"] <- sqrt(vecData$target_x ** 2 + vecData$target_y ** 2)
-    vecData["targetDir"] <- atan2(vecData$target_y,vecData$target_x)
-    vecData[which(vecData$targetDist > spaceSize),"targetDist"] = spaceSize
-    vecData$normTarX <- cos(vecData$targetDir) * vecData$targetDist
-    vecData$normTarY <- sin(vecData$targetDir) * vecData$targetDist
-    # creating target area and forbidden area (only for last step)
-    lastEntry <- tail(vecData,1)
-    dirOkZone <- mkDirOkZone(lastEntry[,c("kick_dir_tol")], variables[[x_var]][["limits"]][2])
-    collisionData <- mkRobot(collisionRadius, collisionForward)
-    dirOkColor      <- cbbPalette[2]
-    stepMinColor   <- cbbPalette[4]
-    stepMaxColor   <- cbbPalette[5]
-    kickColor      <- cbbPalette[3]
-    collisionColor <- cbbPalette[1]
-    # plotting
-    g <- ggplot()
-    # plot kickable area
-    g <- g + geom_polygon(aes(x=x,y=y), kickArea , size = 0, fill= kickColor
-                        , alpha=1)
-    # plot dir ok zone
-    g <- g + geom_polygon(aes(x=x,y=y), dirOkZone, size = 0,
-                          fill= dirOkColor, alpha=0.5)
-    # plot collision area
+    # Plot final robot
+    collisionData <- mkRobotWithPose(config, lastEntry$robot_x, lastEntry$robot_y, lastEntry$robot_dir)
     g <- g + geom_polygon(aes(x=x,y=y), collisionData , size = 0,
                           fill= collisionColor, alpha=1)
-    # plot balls in robot referential
-    g <- g + geom_point(aes(x = ball_x,y = ball_y,
-                            color = step * dt),
-                        vecData,
-                        size = ballSize)
-    # plot target in robot referential
-    g <- g + geom_point(aes(x=normTarX,y=normTarY,
-                            color = step * dt),
+    # plot target
+    g <- g + geom_point(aes(x= target_x, y=target_y),
                         shape="x",
                         vecData, size = targetSize)
+    toTargetData <- data.frame(x=c(lastEntry$robot_x, lastEntry$target_x),
+                               y= c(lastEntry$robot_y, lastEntry$target_y))
+    g <- g + geom_path(aes(x=x,y=y), toTargetData)
+    # plot robot 
+    g <- g + geom_segment(aes(x=robot_x,y=robot_y, xend = robot_end_x, yend = robot_end_y,
+                              color = time),
+                          arrow = arrow(length = unit(0.05,"cm"))
+                          )
+    # plot ball
+    g <- g + geom_point(aes(x=ball_x,y=ball_y,
+                            color = time), size = ballSize)
     # Setting axis
     g <- g + scale_x_continuous(name = "x [m]",
                                 breaks = variables[[x_var]][["breaks"]],
@@ -185,7 +169,7 @@ vectorPlot <- function(data, variables, outputPath)
     ggsave(file = outputPath, g,width=5,height=5)
 }
 
-vectorPlotLasts <- function(path, variables, nbRuns = 10)
+vectorPlotLasts <- function(path, variables, nbRuns = 10, config)
 {
     data <- read.csv(path)
     base <- getBase(path)
@@ -199,17 +183,33 @@ vectorPlotLasts <- function(path, variables, nbRuns = 10)
         run <- lastRuns[rank]
         filteredData <- data[which(data$run == run),]
         dst <- sprintf("%slast_vector_plot_%d_run_%d.png", base, rank, run)
-        vectorPlotTarget(filteredData, variables, dst)
+        vectorPlotTarget(filteredData, variables, dst, config)
     }
 }
 
+option_list <- list(
+    make_option(c("-n","--nb_runs"), type="integer", default=1,
+                help="Number of runs to plot"),
+    make_option(c("-s","--space_size"), type="double", default=1.0,
+                help="Size of the space to show on graphs")
+    )
+parser <- OptionParser(usage="%prog [options] <run_logs.csv> <problem.json>",
+                       option_list = option_list)
+cmd <- parse_args(parser, commandArgs(TRUE), positional_arguments=2)
 
-dt <- 0.1
+if (length(args) < 1) {
+    cat("Usage: ... <logFiles>\n");
+    quit(status=1)
+}
 
-spaceSize <- 3.0
+problem <- fromJSON(file = cmd$args[2])
 
-variables <- list(x      = list(limits = c(-spaceSize, spaceSize)),
-                  y      = list(limits = c(-spaceSize, spaceSize)))
+if (problem[["class name"]] != "SSLDynamicBallApproach") {
+    print("Invalid problem: expecting a SSLDynamicBallApproach problem")
+    quit()
+}
+variables <- list(x      = list(limits = c(-cmd$options$space_size, cmd$options$space_size)),
+                  y      = list(limits = c(-cmd$options$space_size, cmd$options$space_size)))
 # computing breaks
 for (v in names(variables))
 {
@@ -225,16 +225,7 @@ for (v in names(variables))
 }
 
 
-args <- commandArgs(TRUE)
 
-if (length(args) < 1) {
-    cat("Usage: ... <logFiles>\n");
-    quit(status=1)
-}
-
-for (i in 1:length(args)) {
-    path = args[i]
-    vectorPlotLasts(path, variables, 50)
-}
+vectorPlotLasts(cmd$args[1], variables, cmd$options$nb_runs, problem$content)
 
 warnings()
